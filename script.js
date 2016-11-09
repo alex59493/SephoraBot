e8cc0124dee5dc53bcfda6fc4da18ff392ad4462
alex59493 <alexandre.peccaud@gmail.com>
2016-11-09 15:08:26 +0800

    Refactor

--- script.js
+++ script.js
@@ -2,33 +2,16 @@
 
 // CONFIG
 var TIMERESPONSE = 2000; // Number of milliseconds that the bot needs to write its message
+var TIMEBETWEENRESPONSES = 500; // Number of milliseconds between 2 bot messages
  
 var currentAction = null;
 
-// Get the first action and display it
+
+// Start the conversation with the bot
 $(function() {
     var parameters = { currentAction: null }; // Ping the bot with empty request to start the conversation
     sendToBot(parameters, function(res) {
-        // Display greetings
-        var i = 0;
-        var interval = setInterval(function() {
-
-            // If all the item in the greetings array are not yet displayed, display the next one
-            if (i !== res.currentAction.greetings.length) {
-                displayMessage("left", res.currentAction.greetings[i]);
-                i += 1;
-            }
-
-            // Else, display the next question
-            else {
-                clearInterval(interval);
-
-                // Display first question
-                displayMessage("left", res.nextAction.question);
-            }
-
-        }, TIMERESPONSE + 500);
-
+        displayGreetings(res);// Display greetings
         currentAction = res.nextAction;
     });
 });
@@ -44,7 +27,7 @@ $("#userInput").keypress(function(e) {
 
 
 function sendMessage() {
-    displayMessage("right", $("#userInput").val()); // Append user input to displayed messages
+    displayUserMessage($("#userInput").val());
 
     var parameters = { currentAction: currentAction, userInput: $("#userInput").val() };
     sendToBot(parameters, function(res) {
@@ -57,35 +40,57 @@ function sendMessage() {
 }
 
 
+function displayGreetings(res) {
+    var i = 0;
+    var interval = setInterval(function() {
+
+        // If all the item in the greetings array are not yet displayed, display the next one
+        if (i !== res.currentAction.greetings.length) {
+            displayBotMessage(res.currentAction.greetings[i]);
+            i += 1;
+        }
+
+        // Else, display the next question
+        else {
+            clearInterval(interval);
+
+            // Display first question
+            displayBotMessage(res.nextAction.question);
+        }
+
+    }, TIMERESPONSE + TIMEBETWEENRESPONSES);
+}
+
+
 function displayBotResponse(res) {
-    // Display answer to the current entity
     setTimeout(function() {
-        displayMessage("left", res.currentAction.resp);
-    }, 500);
+        // Display answer of the current entity
+        displayBotMessage(res.currentAction.resp);
 
-    // If this is the last entity of the flow, show the bye message and close conversation
-    if (res.nextAction.byeMessage) {
-        setTimeout(function() {
-            displayMessage("left", res.nextAction.byeMessage);
-        }, TIMERESPONSE + 500);
+        // If this is the last entity of the flow, show the bye message and close conversation
+        if (res.nextAction.byeMessage) {
+            setTimeout(function() {
+                displayBotMessage(res.nextAction.byeMessage);
+            }, TIMERESPONSE + TIMEBETWEENRESPONSES);
 
-        currentAction = -1;
-    }
+            currentAction = -1;
+        }
 
-    // Else, display the next question
-    else {
-        setTimeout(function() {
-            displayMessage("left", res.nextAction.question);
-        }, TIMERESPONSE + 500);
+        // Else, display the next question
+        else {
+            setTimeout(function() {
+                displayBotMessage(res.nextAction.question);
+            }, TIMERESPONSE + TIMEBETWEENRESPONSES);
 
-        currentAction = res.nextAction;
-    }
+            currentAction = res.nextAction;
+        }
+    }, TIMEBETWEENRESPONSES);
 }
 
 
 function sendToBot(parameters, callback) {
     $.ajax({
-        url: 'https://4ykhvsc5rb.execute-api.ap-southeast-1.amazonaws.com/test/next',
+        url: 'https://unfxpfonfk.execute-api.ap-southeast-1.amazonaws.com/test/next',
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify(parameters),
@@ -99,25 +104,27 @@ function sendToBot(parameters, callback) {
 }
 
 
-function displayMessage(side, message) {
-    // If the messages emiter is the bot, display the message with the "..." animation
-    if (message && side && side === "left") {
-        $('#messagesWindow').append('<div class="left"><div class="bye"><span>.</span><span>.</span><span>.</span></div><p>' + message + "</p></div>");
-        $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight);
-        // Wait that the message is fully displayed and refresh the scroll
-        // Otherwise, message with more than one line (like "...") won't be completely shown
-        setTimeout(function() {
-            $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight);
-        }, TIMERESPONSE + 1);
-        return true;
-    }
+function displayBotMessage(message) {
+    if (!message) return false;
 
-    // Else if the message emiter is the user, simply display the message (without "...")
-    else if (message && side && side === "right") {
-        $('#messagesWindow').append('<div class="right">' + message + "</div>");
-        $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight);
-        return true;
-    }
+    var waiter = $('<div class="left"><div class="bye"><span>.</span><span>.</span><span>.</span></div></div>').appendTo($('#messagesWindow'));
+    $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight);
 
-    else return false;
+    setTimeout(function() {
+        waiter.remove();
+
+        $('#messagesWindow').append('<div class="left"><p>' + message + "</p></div>");
+        $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight); // Refresh scroll (in case of the height of the paragraphe is higher than the height of the waiter)
+    }, TIMERESPONSE);
+
+    return true;
+}
+
+
+function displayUserMessage(message) {
+    if (!message) return false;
+
+    $('#messagesWindow').append('<div class="right">' + message + "</div>");
+    $('#messagesWindow').scrollTop($('#messagesWindow').get(-1).scrollHeight);
+    return true;
 }
